4e26298be004901aadb041fd9b9600f4db9514db
martinsejas <martinhugosejas@gmail.com>
2022-06-23 13:52:28 -0300

    Definicao de classes feitas

---
+++ src/classes/Turma.ts
@@ -0,0 +1,36 @@
+export class Turma {
+
+    constructor(
+        protected id:string, 
+        public nome:string, 
+        public modulo:number
+    ){}
+
+    getId():string{
+        return this.id; 
+    }
+}
+
+
+
+export class TurmaAtiva extends Turma{
+
+    protected estudantes:number[] = []
+    protected docentes:number[] = []
+    constructor(
+        id:string, 
+        nome:string, 
+        modulo:number, 
+    ){
+        super(id,nome,modulo); 
+        
+    }
+
+    getEstudantes():number[]{
+        return this.estudantes; 
+    }
+
+    getDocentes():number[]{
+        return this.docentes; 
+    }
+}
