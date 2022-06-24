0dc9872af5b2ada8d234c19a6e712c838cb983ea
martinsejas <martinhugosejas@gmail.com>
2022-06-24 10:08:46 -0300

    Adicionei meus endpoints no app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express from 'express'; 
 import cors from "cors"; 
 import * as endpoints from "./endpoints"
+import { registrarTurma } from './endpoints/registrarTurma';
+import { mudarTurmaDeModulo } from './endpoints/mudarTurmaDeModulo';
+import { buscarTurmas } from './endpoints/buscarTurmas';
 
 const app = express(); 
 
 app.use(express.json())
 app.use(cors()); 
 
+app.post("/turma", registrarTurma)
+app.put("/turma/:turmaId", mudarTurmaDeModulo)
+app.get("/turma", buscarTurmas) 
 app.post("/estudante", endpoints.registrarEstudante)
 app.get("/estudante/:nome", endpoints.buscarEstudantePorNome)
 app.put("/estudante/:id", endpoints.redefinirTurmaDoAluno)
