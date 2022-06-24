8afd908f2307896dfcac0f3adab65e9ebfbecdb0
Renata S. Zamboni <86760783+RenataSZamboni@users.noreply.github.com>
2022-06-23 22:56:10 -0300

    endpoint ATUALIZAR turma docente

--- src/data/mudarTurmaDocente.ts
+++ src/data/mudarTurmaDocente.ts
@@ -1,6 +1,6 @@
 import { connection } from "../connection";
 
-export const atualizarTurmaDocente = async (turma_id: string, id:string) => {
+export const mudarTurmaDocente = async (turma_id: string, id:string) => {
     const resultado = await connection("Estudante").update({turma_id}).where({id})
     return resultado
 }
